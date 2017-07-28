88bd217cb2ca314fad2035cf101c8328c583b3cf
wildpow <element1028@gmail.com>
2017-07-28 11:02:26 -0700

    Meal_plan UI
    Put a user interface on it so that a user can create, view, edit, and
    delete his/her own meal plans

--- app/models/meal_plan.rb
+++ app/models/meal_plan.rb
@@ -1,10 +1,12 @@
 class MealPlan < ApplicationRecord
   belongs_to :user
-  has_many :meals
+  has_many :meals, -> { order(:date) }, inverse_of: :meal_plan, dependent: :destroy
 
   validates :start_date, presence: true
   validates :end_date, presence: true
   validates :user, presence: true
+
+  accepts_nested_attributes_for :meals
 
   def build_meals
     user_recipe_ids = user.recipes.pluck(:id)
@@ -17,8 +19,12 @@ class MealPlan < ApplicationRecord
       else
         available_recipe_ids = unused_recipe_ids
       end
-      
+
       meals.build(date: date, recipe_id: available_recipe_ids.sample)
     end
+  end
+
+  def to_s
+    "#{start_date} - #{end_date}"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "welcome#show"
 
-  resources :recipes 
+  resources :recipes
+  resources :meal_plans
 end
